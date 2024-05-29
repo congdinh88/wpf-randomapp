5a4363ff3414b0466e6deecbda478119837db3f0
congdinh88 <congdinh.tk@gmail.com>
2024-05-29 16:46:03 +0700

    29/05/24-2

--- RandomApp/Views/MainWindow.xaml.cs
+++ RandomApp/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using RandomApp.Pages;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,21 +18,55 @@ namespace RandomApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<string> item {get;set;}
         public MainWindow()
         {
             InitializeComponent();
             MainContent.Navigate(new Page1());
-
+            comboBox.ItemsSource = PageShow();
+            comboBox.Text=PageShow().FirstOrDefault()?.ToString();
         }
 
+        private string[] PageShow()
+        {
+            string[] strArray =
+            {
+                "Page1",
+                "Page2",
+                "Page3",
+            };
+            return strArray;
+        }
         public void NavigateToPage2()
         {
             MainContent.Navigate(new Page2());
+            comboBox.Text = PageShow()[1].ToString();
         }
 
         public void NavigateToPage3()
         {
             MainContent.Navigate(new Page3());
+            comboBox.Text = PageShow()[2].ToString();
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string s = comboBox.Text;
+            if (s != null)
+            {
+                switch (s)
+                {
+                    case "Page1":
+                        MainContent.Navigate(new Page1());
+                        break;
+                    case "Page2":
+                        MainContent.Navigate(new Page2());
+                        break;
+                    case "Page3":
+                        MainContent.Navigate(new Page3());
+                        break;
+                }
+            }
         }
     }
 }
